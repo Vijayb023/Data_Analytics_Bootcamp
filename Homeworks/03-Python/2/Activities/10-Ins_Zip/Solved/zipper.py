7265c3e6face9f9fabebfefeffe8730530688cc1
Vijayb023 <vijaypb024@gmail.com>
2019-06-22 13:42:26 -0400

    Python practice with terminal

--- Homeworks/03-Python/2/Activities/10-Ins_Zip/Solved/zipper.py
+++ Homeworks/03-Python/2/Activities/10-Ins_Zip/Solved/zipper.py
@@ -8,20 +8,20 @@ department = ["Boss", "Sales", "Sales", "HR"]
 
 # Zip all three lists together into tuples
 roster = zip(indexes, employees, department)
-
+print(roster)
 # save the output file path
 output_file = os.path.join("output.csv")
 
 # open the output file, create a header row, and then write the zipped object to the csv
-with open(output_file, "w", newline="") as datafile:
-    writer = csv.writer(datafile)
+# with open(output_file, "w", newline="") as datafile:
+#     writer = csv.writer(datafile)
 
-    writer.writerow(["Index", "Employee", "Department"])
+#     writer.writerow(["Index", "Employee", "Department"])
 
-    writer.writerows(roster)
+#     writer.writerows(roster)
 
 
 # # to print out to terminal:
 # #comment out above code and run the code below
-# for employee in roster:
-#     print(employee)
+for employee in roster:
+     print(employee)

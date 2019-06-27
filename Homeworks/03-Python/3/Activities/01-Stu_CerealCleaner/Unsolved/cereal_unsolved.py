31761d56199a3d351ffb7badd99b90e9f2f64c55
Vijayb023 <vijaypb024@gmail.com>
2019-06-27 18:59:54 -0400

    Python work

--- Homeworks/03-Python/3/Activities/01-Stu_CerealCleaner/Unsolved/cereal_unsolved.py
+++ Homeworks/03-Python/3/Activities/01-Stu_CerealCleaner/Unsolved/cereal_unsolved.py
@@ -2,3 +2,13 @@ import os
 import csv
 
 cereal_csv = os.path.join("../Resources", "cereal.csv")
+
+with open(cereal_csv, newline="") as csvfile: 
+    csvreader = csv.reader(csvfile, delimiter=",")
+
+    csv_header = next(csvfile)
+    print(f"header:{csv_header}")
+    for row in csvreader:
+
+        if float(row[7]) >= 5:
+            print(row)
--- Homeworks/03-Python/3/Activities/03-Stu_HobbyBook/Unsolved/HobbyBook_Unsolved.py
+++ Homeworks/03-Python/3/Activities/03-Stu_HobbyBook/Unsolved/HobbyBook_Unsolved.py
@@ -1,1 +1,16 @@
 # @TODO: Your code here
+About_me = {
+    "name":"Vijay Bagavatula",
+    "age": 19,
+    "Hobbies":[
+        "Basketball",
+        "Weight training",
+        "Tennis",
+        "Reading"
+    ],
+    "wake_up":{
+        "weekdays":5,
+        "weekends":10
+    }
+    }
+print(f'Hello my name is {About_me["name"]}. I have {len(About_me["Hobbies"])} hobbies. My hobbies include {About_me["Hobbies"]}. I wake up at {About_me["wake_up"]["weekdays"]}')

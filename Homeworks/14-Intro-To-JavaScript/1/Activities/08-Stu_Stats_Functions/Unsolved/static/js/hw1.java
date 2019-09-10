f594a21152184181a7dfc95c9ecefc788bfa83f6
Vijayb023 <vijaypb024@gmail.com>
2019-09-10 18:39:07 -0400

    update

---
+++ Homeworks/14-Intro-To-JavaScript/1/Activities/08-Stu_Stats_Functions/Unsolved/static/js/hw1.java
@@ -0,0 +1,9 @@
+public class Loops{
+    publicstaticvoidmain(String[] args){
+        for(inti=1; i<=10; i++){
+            for(intj=1; j<=10-i; j++){
+                System.out.print(" ");
+            }
+             for(intj=1; j<=2*i-1; j++) {
+                 System.out.print("*");}
+System.out.println();}}}
